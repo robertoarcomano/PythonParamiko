51e5606dd6942856eb3c1f96e908254717122d48
Roberto Arcomano <bertolinux@gmail.com>
2020-03-17 18:29:07 +0000

    With Threads

--- test.py
+++ test.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python
 import Utilities
+import threading
+import time
+import random
 
-connection = Utilities.SSHConnection("localhost", "berto", "/home/berto/.ssh/id_rsa")
-result, error = connection.exec("find /etc")
-for line in result:
-    print(line.rstrip())
+
+def exec_command(command):
+    connection = Utilities.SSHConnection("localhost", "berto", "/home/berto/.ssh/id_rsa")
+    result, error = connection.exec(command)
+    for line in result:
+        print("Command: " + command + " line: " + line.rstrip())
+        time.sleep(random.random())
+
+
+commands = ["find /usr", "find /var"]
+threads = list()
+for command in commands:
+    threads.append(threading.Thread(target=exec_command, args=(command,)))
+
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
